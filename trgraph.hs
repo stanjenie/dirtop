660443bbd911b1fcf35e8927364eaa3df77f05c8
Deva Devanandan <31284342+stanjenie@users.noreply.github.com>
2024-09-08 09:55:51 -0400

    Update trgraph.hs
    Added P and V operations, with function [work] computing resource consumption of conservative programs and example [scale].

--- trgraph.hs
+++ trgraph.hs
@@ -1,6 +1,9 @@
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 data AExp = Var String | Val Int | Add AExp AExp | Mult AExp AExp | Custom String AExp AExp deriving(Show)
 data BExp = True | False | Less AExp AExp | And BExp BExp | Not BExp deriving(Show)
-data Prog = Skip | Let String AExp | Seq Prog Prog | Cond BExp Prog Prog | While BExp Prog | Para Prog Prog deriving(Show)
+data Prog = Skip | P String | V String | Let String AExp | Seq Prog Prog | Cond BExp Prog Prog | While BExp Prog | Para Prog Prog deriving(Show)
 
 
 data Graph = Graph
@@ -19,6 +22,8 @@ graphvis (Graph n e) = do
 
 trgraph :: Prog -> Graph
 trgraph Skip = Graph 1 []
+trgraph (P _) = Graph 2 [[0,1]]
+trgraph (V _) = Graph 2 [[0,1]]
 trgraph (Let _ _) = Graph 2 [[0,1]]
 trgraph (Seq p1 p2) = let g1 = trgraph p1 in
   let g2 = trgraph p2 in
@@ -33,6 +38,30 @@ trgraph (Para p1 p2) = let g1 = trgraph p1 in
   let leib1 = map (\i -> map (\e -> map (\n -> n + (i * nodes g1)) e) (edges g1)) [0..nodes g2 - 1] in
   let leib2 = map (\i -> map (\e -> map (\n -> i + (n * nodes g1)) e) (edges g2)) [0..nodes g1 - 1] in
   Graph (nodes g1 * nodes g2) (concat leib1 ++ concat leib2)
+
+work_eq :: Map String Int -> Map String Int -> Bool
+work_eq a b = (Map.isSubmapOf a b) && (Map.isSubmapOf b a)
+
+work :: Prog -> Maybe (Map String Int)
+work Skip = Just Map.empty
+work (Let _ _) = Just Map.empty
+work (P a) = Just (Map.singleton a (-1))
+work (V a) = Just (Map.singleton a 1)
+work (Seq p q) = do 
+  wp <- work p
+  wq <- work q
+  Just (Map.unionWith (+) wp wq)
+work (Cond _ p q) = do
+  wp <- work p
+  wq <- work q
+  if (work_eq wp wq) then work p else Nothing
+work (While _ p) = do
+  wp <- work p
+  if (Map.null wp) then Just Map.empty else Nothing
+work (Para p q) = do
+  wp <- work p
+  wq <- work q
+  Just (Map.unionWith (+) wp wq)
 
 b_or :: BExp -> BExp -> BExp
 b_or b1 b2 = Not (And (Not b1) (Not b2))
@@ -51,6 +80,20 @@ syracuse = Seq
         (Let "x" (Custom "/" (Var "x") (Val 2)))))
     (Let "print" (Custom "string:[Reached 1]" (Val 0) (Val 0))))
 
+scale = Seq
+  (Let "d" (Val 1))
+  (Para 
+    (Seq (P "a") (Seq (Let "d" (Val 0)) (V "a")))
+    (Seq (P "a") (Seq 
+      (Cond (b_neq (Var "d") (Val 0)) 
+        (Let "y" (Custom "/" (Var "x") (Var "d"))) 
+        (Let "y" (Var "x"))) 
+      (V "a"))))
 
 
-main = graphvis (trgraph syracuse)
+
+
+main = do 
+  graphvis (trgraph syracuse)
+  print (work syracuse)
+  print (work scale)
