7446fc1fd24a15a59b3c669494bfc8f154025f12
Deva Devanandan <31284342+stanjenie@users.noreply.github.com>
2024-09-08 10:39:05 -0400

    Update trgraph.hs
    fixed bug with map equality checking, added while-loop example

--- trgraph.hs
+++ trgraph.hs
@@ -2,7 +2,7 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 
 data AExp = Var String | Val Int | Add AExp AExp | Mult AExp AExp | Custom String AExp AExp deriving(Show)
-data BExp = True | False | Less AExp AExp | And BExp BExp | Not BExp deriving(Show)
+data BExp = BTrue | BFalse | Less AExp AExp | And BExp BExp | Not BExp deriving(Show)
 data Prog = Skip | P String | V String | Let String AExp | Seq Prog Prog | Cond BExp Prog Prog | While BExp Prog | Para Prog Prog deriving(Show)
 
 
@@ -39,8 +39,11 @@ trgraph (Para p1 p2) = let g1 = trgraph p1 in
   let leib2 = map (\i -> map (\e -> map (\n -> i + (n * nodes g1)) e) (edges g2)) [0..nodes g1 - 1] in
   Graph (nodes g1 * nodes g2) (concat leib1 ++ concat leib2)
 
+work_zero :: Map String Int -> Bool
+work_zero = (foldr (\a b -> b && (a == 0)) True) . Map.elems
 work_eq :: Map String Int -> Map String Int -> Bool
-work_eq a b = (Map.isSubmapOf a b) && (Map.isSubmapOf b a)
+work_eq a b = (work_zero (a Map.\\ b)) && (work_zero (b Map.\\ a))
+  && (work_zero (Map.intersectionWith (-) a b))
 
 work :: Prog -> Maybe (Map String Int)
 work Skip = Just Map.empty
@@ -57,7 +60,7 @@ work (Cond _ p q) = do
   if (work_eq wp wq) then work p else Nothing
 work (While _ p) = do
   wp <- work p
-  if (Map.null wp) then Just Map.empty else Nothing
+  if (work_zero wp) then Just Map.empty else Nothing
 work (Para p q) = do
   wp <- work p
   wq <- work q
@@ -90,10 +93,12 @@ scale = Seq
         (Let "y" (Var "x"))) 
       (V "a"))))
 
-
+block :: String -> Prog -> Prog
+block a p = Seq (P a) (Seq p (V a))
 
 
 main = do 
   graphvis (trgraph syracuse)
   print (work syracuse)
   print (work scale)
+  print (work (While BTrue (block "a" Skip)))
