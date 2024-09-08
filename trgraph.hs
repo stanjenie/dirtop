bb410d9dfe12cd449995e3ca72b41b40f9ea2819
Deva Devanandan <31284342+stanjenie@users.noreply.github.com>
2024-09-08 08:41:46 -0400

    Update trgraph.hs
    Added semantic hooks

--- trgraph.hs
+++ trgraph.hs
@@ -1,4 +1,7 @@
-data Prog = Skip | Act String | Seq Prog Prog | Cond String Prog Prog | While String Prog | Para Prog Prog deriving(Show)
+data AExp = Var String | Val Int | Add AExp AExp | Mult AExp AExp | Custom String AExp AExp deriving(Show)
+data BExp = True | False | Less AExp AExp | And BExp BExp | Not BExp deriving(Show)
+data Prog = Skip | Let String AExp | Seq Prog Prog | Cond BExp Prog Prog | While BExp Prog | Para Prog Prog deriving(Show)
+
 
 data Graph = Graph
   {nodes :: Int
@@ -16,7 +19,7 @@ graphvis (Graph n e) = do
 
 trgraph :: Prog -> Graph
 trgraph Skip = Graph 1 []
-trgraph (Act _) = Graph 2 [[0,1]]
+trgraph (Let _ _) = Graph 2 [[0,1]]
 trgraph (Seq p1 p2) = let g1 = trgraph p1 in
   let g2 = trgraph p2 in
   Graph (nodes g1 + nodes g2 - 1) (edges g1 ++ (map (\e -> map (\n -> n + nodes g1 - 1) e) (edges g2)))
@@ -31,6 +34,23 @@ trgraph (Para p1 p2) = let g1 = trgraph p1 in
   let leib2 = map (\i -> map (\e -> map (\n -> i + (n * nodes g1)) e) (edges g2)) [0..nodes g1 - 1] in
   Graph (nodes g1 * nodes g2) (concat leib1 ++ concat leib2)
 
-syracuse = Seq (Act "x := 5") (Seq (While "x != 1" (Cond "x mod 2 != 0" (Seq (Act "x := 3 * x") (Act "x := x + 1")) (Act "x := x/2"))) (Act "print 'Reached 1!'"))
+b_or :: BExp -> BExp -> BExp
+b_or b1 b2 = Not (And (Not b1) (Not b2))
+b_neq :: AExp -> AExp -> BExp
+b_neq a1 a2 = b_or (Less a1 a2) (Less a2 a1)
+
+
+syracuse = Seq 
+  (Let "x" (Val 5)) 
+  (Seq 
+    (While (b_neq (Var "x") (Val 1)) 
+      (Cond (b_neq (Custom "mod" (Var "x") (Val 2)) (Val 0)) 
+        (Seq
+          (Let "x" (Mult (Val 3) (Var "x"))) 
+          (Let "x" (Add (Var "x") (Val 1)))) 
+        (Let "x" (Custom "/" (Var "x") (Val 2)))))
+    (Let "print" (Custom "string:[Reached 1]" (Val 0) (Val 0))))
+
+
 
 main = graphvis (trgraph syracuse)
